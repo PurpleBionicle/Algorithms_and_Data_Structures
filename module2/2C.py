1a64fb9b1dcc7fad2d90250601354d359a35d80d
88888812 <ndp2012@yandex.ru>
2022-11-04 20:27:25 +0300

    2C some fixes and added print

--- module2/2C.py
+++ module2/2C.py
@@ -1,5 +1,6 @@
 import fileinput
 import re
+from typing import Any
 
 
 class Heap():
@@ -13,15 +14,13 @@ class Heap():
         return (index - 1) >> 1
 
     def __left_child(self, index: int) -> int:
+        #   0
+        #  1   2
+        # 3 4 5 6
         return (index << 1) + 1
 
     def __right_child(self, index: int) -> int:
         return (index << 1) + 2
-
-    def __find_key(self, index: int) -> int:
-        for node in self.tree:
-            if self.hash_table[node[0]] == index:
-                return node[0]
 
     def __heapify_up(self, index: int) -> None:
         # Поднимаем
@@ -31,48 +30,163 @@ class Heap():
         parent: int = self.__parent(index)
         while index != 0 or index < self.__parent(index):
             # поменяли в хэше индексы для ключей
-            self.hash_table[self.__find_key(index)], \
-            self.hash_table[self.__find_key(parent)] = parent, index
+            self.hash_table[self.tree[index][0]], \
+            self.hash_table[self.tree[index][parent]] = parent, index
             # по индексам поменяем значения и ключи
-            self.tree[index], self.tree[parent] = [self.__find_key(parent), self.tree[parent][1]], \
-                                                  [self.__find_key(index), self.tree[index][1]]
+            self.tree[index], self.tree[parent] = [self.tree[index][parent], self.tree[parent][1]], \
+                                                  [self.tree[index][0], self.tree[index][1]]
 
             index = parent
             parent = self.__parent(parent)
 
-    # TODO
     def __heapify_down(self, index: int) -> None:
         left: int = self.__left_child(index)
         right: int = self.__right_child(index)
-        # куда спускаем ????
+        while left < len(self.tree):
+            if right < len(self.tree):  # если есть оба ребенка
+                # если дети больше = дошли до куда надо
+                if self.tree[index][0] < self.tree[left][0] and \
+                        self.tree[index][0] < self.tree[right][0]:
+                    break
+
+                elif self.tree[left][0] < self.tree[right][0]:  # если правый больше, то поменяем с левом
+                    self.hash_table[self.tree[index][0]], self.hash_table[self.tree[left][0]] = \
+                        left, index
+                    self.tree[left], self.tree[index] = self.tree[index], self.tree[left]
+                    index = left
+                else:  # если левый больше, то поменяем с правом
+                    self.hash_table[self.tree[index][0]], self.hash_table[self.tree[right][0]] = \
+                        right, index
+                    self.tree[right], self.tree[index] = self.tree[index], self.tree[right]
+                    index = right
+            else:  # если только левый есть
+                if self.tree[index][0] < self.tree[left][0]:
+                    break
+                else:
+                    self.hash_table[self.tree[index][0]], self.hash_table[self.tree[left][0]] = \
+                        left, index
+                    self.tree[left], self.tree[index] = self.tree[index], self.tree[left]
+                    index = left
+
+            left: int = self.__left_child(index)
+            right: int = self.__right_child(index)
 
     def add(self, key: int, value: str) -> None:
-        pass
+        self.tree.append([key, value])
+        self.hash_table[key] = value
+        self.__heapify_up(len(self.tree) - 1)
 
     def delete(self, key: int) -> None:
-        pass
+        if len(self.tree) == 0 or self.__search(key) == -1:
+            print('error')
+            return
+        # если есть
+        # поменяли в хэше индексы для ключей
+        index: int = self.hash_table[key]
+        self.tree[index], self.tree[-1] = self.tree[-1], self.tree[index]
 
-    def set(self, key: int, value: str) -> None:
-        pass
+        self.tree.pop()
+        if self.tree[index][1] < self.tree[self.__parent(index)][1]:
+            self.__heapify_up(index)
+        else:
+            self.__heapify_down(index)
 
-    def __search(self, key: int) -> list:
-        pass
+        del self.hash_table[key]
+
+    def set(self, key: int, value: str) -> int:
+        index: int = self.__search(key)[0]
+        if index == -1:
+            print('error')
+            return -1
+        self.hash_table[key] = index
+        self.tree[index][1] = value
+        return 1
+
+    def __search(self, key: int) -> list[int] | tuple[int, Any]:
+        index: int = self.hash_table[key]
+        if index is None:
+            print('error')
+            return [-1]
+        else:
+            # индекс + значение
+            return index, self.tree[index][1]
 
     def search(self, key: int) -> None:
         result: list = self.__search(key)
-        print('1 {0} {1}'.format(list[0],list[1]) if result[0] != -1 else '0')
+        print('1 {0} {1}'.format(list[0], list[1]) if result[0] != -1 else '0')
 
-    def min(self)->None:
-        pass
+    def min(self) -> None:
+        if len(self.tree) == 0:
+            print('error')
+        else:
+            print(self.tree[0])
 
-    def max(self)->None:
-        pass
+    def max(self) -> tuple[int, int, Any] | None:
+        if len(self.tree) == 0:
+            print('error')
+            return
+        current_max: list = []
+        for i in range(len(self.tree), 0, -1):
+            # смотрим только листья дерева
+            if self.tree[self.__left_child(i)] is not None or \
+                    self.tree[self.__right_child(i)] is not None:
+                return current_max[1], i + len(self.tree), current_max[0]
 
-    def print(self)->None:
-        pass
+            if self.tree[i] > current_max:
+                current_max = self.tree
 
-    def extract(self)->None:
-        pass
+    def extract(self) -> list:
+        if len(self.tree) == 0:
+            print('error')
+            return
+        # если есть
+        result: list = self.tree[0]
+        # поменяли в хэше индексы для ключей
+        self.hash_table[self.tree[-1][0]] = 0
+
+        self.tree[0], self.tree[-1] = self.tree[-1], self.tree[0]
+        self.tree.pop()
+        self.__heapify_down(0)
+        del self.hash_table[result[0]]
+        return result
+
+    # TODO
+    def print(self) -> None:
+        def __print_(count: int) -> None:
+            for i in range(count):
+                print(' _', end='')
+
+        if len(self.tree) == 0:
+            print('_')
+            return
+
+        print('[{0} {1}]'.format(self.tree[0][0], self.tree[0][1]), end='')
+
+        index: int = 0
+        level_size: int = 2
+        while index < len(self.tree):
+            print('')  # \n
+            if index + level_size < len(self.tree):
+                for level_position in range(level_size):
+                    if level_position != 0:
+                        print(' ', end='')
+                        current_elem: list = self.tree[index + level_position]
+                        parent: int = self.__parent(index + level_position)
+                        print('[{0} {1} {2}]'.format(current_elem[0], current_elem[1], self.tree[parent][0]), end='')
+            else:
+                diff: int = len(self.tree) - index
+                for level_position in range(diff):
+                    if level_position != 0:
+                        print(' ', end='')
+                    current_elem: list = self.tree[index + level_position]
+                    parent: int = self.__parent(index + level_position)
+                    print('[{0} {1} {2}]'.format(current_elem[0], current_elem[1], self.tree[parent][0]), end='')
+
+                __print_(level_size - diff)
+
+            index += level_size
+            level_size <<= 1
+
 
 if __name__ == '__main__':
 
