d061381db9ff1cd5f4e215919cf3763e01ed386d
88888812 <ndp2012@yandex.ru>
2022-12-22 18:48:25 +0300

    1C some fixes

--- module1/1C.py
+++ module1/1C.py
@@ -1,62 +1,53 @@
 import fileinput
 
 
-class Graph():
+def searching(graph: dict, searching_element: str, way: list[str], start_libraries: list[str], visited: dict) -> None:
+    if graph.get(searching_element):
+        for step in graph.get(searching_element):
+            if step not in visited:
+                way.append(step)
+                visited[step] = 1
+                searching(graph, step, way, start_libraries, visited)
 
-    def __make_graph(self, line: str) -> None:
-        libs = line.split(' ')
-        # начинаем с 2
-        for lib in libs[1:]:
-            if lib in self.graph:
-                # если нет либы в ее зависимости то строим
-                if libs[0] not in self.graph[lib]:
-                    self.graph[lib].append(libs[0])
+    if len(way) > 1 and way[len(way) - 1] in start_libraries:
+        for g in reversed(way):
+            if g != way[0]:
+                print(g, end=' ')
             else:
-                self.graph[lib] = [libs[0]]
+                print(g)
+    if len(way) != 0:
+        visited.pop(way.pop(), None)
 
-    def __add_dependence(self) -> None:
-        for node in self.dependence:
-            if node in self.graph:
-                if node not in self.graph[node]:
-                    self.graph[node].append(node)
-            else:
-                self.graph[node] = [node]
 
-    def __init__(self):
-        self.graph: dict = {}
-        self.vuln_libs = set(input().split(' '))
-        self.dependence = set(input().split(' '))
-        for line in fileinput.input():
-            line = line.replace('\n', '')
-            # построим граф
-            self.__make_graph(line)
+if __name__ == '__main__':
+    graph: dict = {}
+    vuln_libs: list = []
+    dependence: list = []
+    while len(vuln_libs) == 0:
+        vuln_libs = list(set(input().strip().split(' ')))
+    while len(dependence) == 0:
+        dependence = list(set(input().strip().split(' ')))
 
-        self.__add_dependence()
-
-    def search_vulnerability(self) -> None:
-        for vuln_lib in self.vuln_libs:
-            path: list = [vuln_lib]
-            visited: dict = {vuln_lib: True}
-            # поиск вглубину
-            self.__dfs(vuln_lib, path, visited)
-
-    def __dfs(self, vuln_lib: str, path: list, used: dict) -> None:
-        # если есть в зависимости с нашей либой
-        if vuln_lib in self.graph:
-            # идем по ребрам от уязвимой - от нее "вверх", а не вниз к ней
-            for parent in self.graph[vuln_lib]:
-                # если спустились до нее
-                if vuln_lib == parent and vuln_lib in self.dependence:
-                    path.reverse()
-                    print(" ".join(path))
-                    return
+    for line in fileinput.input():
+        libraries: list = line.strip().split(' ')
+        if len(libraries) != 0:
+            main_lib: str = libraries.pop(0)
+            for lib in libraries:
+                if lib in graph:
+                    again: bool = False
+                    for vertex in graph[lib]:
+                        if vertex == main_lib:
+                            again = True
+                    if not again:
+                        graph[lib].append(main_lib)
                 else:
-                    if parent not in used:
-                        used[parent] = True
-                        self.__dfs(parent, path + [parent], used)
-                        used.pop(parent)
+                    # добавим ребра в наш граф
+                    graph[lib] = [main_lib]
 
-
-if __name__ == "__main__":
-    graph = Graph()
-    graph.search_vulnerability()
+    common = [value for value in vuln_libs if value in dependence]
+    while len(common) != 0:
+        print(common.pop())
+    for vuln_lib in vuln_libs:
+        way = [vuln_lib]
+        visited = {vuln_lib: 1}
+        searching(graph, vuln_lib, way, dependence, visited)
