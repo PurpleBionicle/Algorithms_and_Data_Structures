f859b863a4b2b82241e4b2630bc9f224ec8b852f
88888812 <ndp2012@yandex.ru>
2023-01-03 14:53:56 +0300

    1C some fix

--- module1/1C.py
+++ module1/1C.py
@@ -1,53 +1,78 @@
 import fileinput
 
 
-def searching(graph: dict, searching_element: str, way: list[str], start_libraries: list[str], visited: dict) -> None:
-    if graph.get(searching_element):
-        for step in graph.get(searching_element):
-            if step not in visited:
-                way.append(step)
-                visited[step] = 1
-                searching(graph, step, way, start_libraries, visited)
+class Graph():
 
-    if len(way) > 1 and way[len(way) - 1] in start_libraries:
-        for g in reversed(way):
-            if g != way[0]:
-                print(g, end=' ')
+    def add_vertex(self, libs: list[str]) -> None:
+        # построим граф
+        # начинаем с 2
+        for lib in libs[1:]:
+            if lib in self.graph:
+                again: bool = False
+                for node in self.graph[lib]:
+                    if node == libs[0]:
+                        again = True
+                # если нет либы в ее зависимости то строим
+                if not again:
+                    self.graph[lib].append(libs[0])
             else:
-                print(g)
-    if len(way) != 0:
-        visited.pop(way.pop(), None)
+                self.graph[lib] = [libs[0]]
+
+    def __add_dependence(self) -> None:
+        for node in self.dependence:
+            if node in self.graph:
+                if node not in self.graph[node]:
+                    self.graph[node].append(node)
+            else:
+                self.graph[node] = [node]
+
+    def __init__(self, vuln_libs: list[str], dependence: list[str]):
+        self.graph: dict = {}
+        self.vuln_libs = vuln_libs
+        self.dependence = dependence
+
+        self.__add_dependence()
+
+    def search_vulnerability(self) -> None:
+        common = [lib for lib in vuln_libs if lib in dependence]
+        while len(common) != 0:
+            print(common.pop())
+
+        for vuln_lib in self.vuln_libs:
+            path: list[str] = [vuln_lib]
+            visited: dict = {vuln_lib: True}
+            # поиск вглубину
+            self.__dfs(vuln_lib, path, visited)
+
+    def __dfs(self, vuln_lib: str, path: list, used: dict) -> None:
+        if self.graph.get(vuln_lib):
+            for step in self.graph.get(vuln_lib):
+                if step not in used:
+                    path.append(step)
+                    used[step] = True
+                    self.__dfs(step, path, used)
+
+        if len(path) > 1 and path[len(path) - 1] in self.dependence:
+            for node in reversed(path):
+                if node != path[0]:
+                    print(node, end=' ')
+                else:
+                    print(node)
+        if len(path) != 0:
+            used.pop(path.pop(), None)
 
 
-if __name__ == '__main__':
-    graph: dict = {}
-    vuln_libs: list = []
-    dependence: list = []
+if __name__ == "__main__":
+    vuln_libs: list[str] = []
+    dependence: list[str] = []
     while len(vuln_libs) == 0:
         vuln_libs = list(set(input().strip().split(' ')))
     while len(dependence) == 0:
         dependence = list(set(input().strip().split(' ')))
 
+    graph: Graph = Graph(vuln_libs, dependence)
     for line in fileinput.input():
-        libraries: list = line.strip().split(' ')
-        if len(libraries) != 0:
-            main_lib: str = libraries.pop(0)
-            for lib in libraries:
-                if lib in graph:
-                    again: bool = False
-                    for vertex in graph[lib]:
-                        if vertex == main_lib:
-                            again = True
-                    if not again:
-                        graph[lib].append(main_lib)
-                else:
-                    # добавим ребра в наш граф
-                    graph[lib] = [main_lib]
+        libs: list[str] = line.strip().split(' ')
+        graph.add_vertex(libs)
 
-    common = [value for value in vuln_libs if value in dependence]
-    while len(common) != 0:
-        print(common.pop())
-    for vuln_lib in vuln_libs:
-        way = [vuln_lib]
-        visited = {vuln_lib: 1}
-        searching(graph, vuln_lib, way, dependence, visited)
+    graph.search_vulnerability()
